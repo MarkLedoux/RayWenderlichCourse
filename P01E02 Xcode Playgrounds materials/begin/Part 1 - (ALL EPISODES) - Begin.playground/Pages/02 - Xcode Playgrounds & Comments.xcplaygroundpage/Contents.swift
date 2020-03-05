4e490b0325be5ea5c2153fd55989b4f0d703c74d
vinceled <mark.ledoux@live.fr>
2020-03-05 20:43:33 +0100

    first videos

---
+++ P01E02 Xcode Playgrounds materials/begin/Part 1 - (ALL EPISODES) - Begin.playground/Pages/02 - Xcode Playgrounds & Comments.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [⇐ Previous: 01 - Introduction](@previous)
+//: ## Episode 02: Xcode Playgrounds
+import Foundation
+
+var str = "Hello, playground"
+
+let goodByeMessage = "See you soon!"
+
+print(goodByeMessage)
+//: [⇒ Next: 03 - Booleans & Comparison Operators](@next)
+
---
+++ P01E02 Xcode Playgrounds materials/begin/Part 1 - (ALL EPISODES) - Begin.playground/Pages/07 - Optionals.xcplaygroundpage/Contents.swift
@@ -0,0 +1,7 @@
+//: [⇐ Previous: 07 - Optionals](@previous)
+//: ## Episode 07: Optionals
+
+var petName: String?
+
+petName = "Mango"
+//: [⇒ Next: 08 - Challenge - Optionals](@next)
